06312f36ab9ff00d46d51b21a78188702db4e261
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-08-21 20:33:30 -0600

    Add Curso/CrecimientoModel.js

---
+++ server/models/cursoCreciModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cursoCreciSchema = new Schema({
+  nuevo: Boolean,
+  fechainicio: Date,
+  fechaFinal: Date,
+  ofrenda: Number,
+  horario: String,
+  ubicacion: String,
+  dirigidoA: String,
+  dirigidoPor: String,
+  tipo: String,
+  estado: Boolean,
+});
+
+module.exports = mongoose.model("CursoCreci", cursoCreciSchema);
