f4b8102a5a9c26f5051048db41961c312ba1ec3a
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-08-22 12:17:56 -0600

    Add middlewares for token

---
+++ server/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  try {
+    const validPayload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(validPayload);
+    // Llamar a next solo si el token es válido
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token no válido, no estás autorizado" });
+  }
+};
+
+module.exports = authenticateToken;
