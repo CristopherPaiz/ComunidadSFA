8b76335ad06837c378ba890bf7b96852f498dcfd
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-08-23 15:48:40 -0600

    UserModel fields require added

--- server/models/User/userModel.js
+++ server/models/User/userModel.js
@@ -2,12 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const usuarioSchema = new Schema({
-  nombre: String,
-  rol: String,
-  username: { type: String, unique: true },
-  contrasenia: String,
+  nombre: {
+    type: String,
+    require: true,
+  },
+  rol: {
+    type: String,
+    require: true,
+  },
+  username: {
+    require: true,
+    type: String,
+    unique: true,
+  },
+  contrasenia: {
+    type: String,
+    require: true,
+  },
   foto: String,
-  estado: { type: Boolean, default: true },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
