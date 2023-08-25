f9d4cde227cdb3dfe41953e4d67537b6337475a2
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-08-25 11:15:02 -0600

    Protect route for Comunidad === "Admin"

--- client/src/views/Comunidad.jsx
+++ client/src/views/Comunidad.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { contexto } from "../context/ContextProvider";
+import { Navigate } from "react-router-dom";
 import Navbutton from "../components/Navbutton";
 
 const Comunidad = () => {
-  return (
-    <>
-      <Navbutton />
-    </>
-  );
+  const { loggedIn, usuario } = useContext(contexto);
+
+  const verificarExpiracionToken = () => {
+    const expirationDate = localStorage.getItem("miTokenExpiration");
+    if (expirationDate) {
+      const now = new Date();
+      const expired = now >= new Date(expirationDate);
+      if (expired) {
+        // El token ha expirado, borrarlo del LocalStorage
+        localStorage.removeItem("usuarioSFA");
+        localStorage.removeItem("loggedSFA");
+        localStorage.removeItem("demasdatosSFA");
+        localStorage.removeItem("miTokenExpiration");
+      }
+    }
+  };
+
+  useEffect(() => {
+    verificarExpiracionToken();
+  }, []);
+
+  if (loggedIn && usuario.rol === "Admin") {
+    return (
+      <>
+        <Navbutton />
+      </>
+    );
+  } else {
+    return <Navigate to={"/login"} />; // Redirige a la página de inicio
+  }
 };
 
 export default Comunidad;
