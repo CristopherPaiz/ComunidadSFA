78bf533d13638d4caa457a8d101fd1427bf5e9f1
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-09-06 09:05:42 -0600

    some bug fix

--- client/src/components/NavButtonFarmacia.jsx
+++ client/src/components/NavButtonFarmacia.jsx
@@ -3,16 +3,13 @@ import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 import F_agregar_producto from "../forms/Farmacia/F_agregar_producto";
 import F_venta_producto from "../forms/Farmacia/F_venta_producto";
 import F_compra_producto from "../forms/Farmacia/F_compra_producto";
-import CardExample from "../forms/CardExample";
 
 const NavbuttonFarmacia = () => {
   const [selected, setSelected] = useState("");
 
   return (
     <div className="flex w-full px-2 flex-col m-auto sm:w-11/12">
-      <p className="m-auto sm:hidden mb-3 mt-1 animate-bounceEdit">
-        ← Desliza para ver las demás opciones →{" "}
-      </p>
+      <p className="m-auto sm:hidden mb-3 mt-1 animate-bounceEdit">← Desliza para ver las demás opciones → </p>
       <Tabs
         aria-label="Options"
         selectedKey={selected}
--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Link as RouterLink } from "react-router-dom";
 import comunidad from "/comunidad.svg";
 import { Button } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
@@ -19,8 +18,6 @@ const Login = () => {
     if (usuarioLS && loggedLS) {
       setLoggedIn(true);
       setUsuario(JSON.parse(demasDatosLS));
-    } else {
-      null;
     }
   }, []);
 
@@ -36,84 +33,82 @@ const Login = () => {
     }
   };
 
-  //aqui solo escuchamos los cambios de los inputs de usuario
   const handleInputChangeUser = (event) => {
     setNombreUsuario(event.target.value);
   };
 
-  //aqui solo escuchamos los cambios de los inputs de password
   const handleInputChangePassword = (event) => {
     setPassword(event.target.value);
   };
 
-  if (loggedIn) {
-    if (usuario.rol === "Admin") {
-      navigate("/comunidad");
-    } else {
-      navigate("/farmacia");
+  useEffect(() => {
+    if (loggedIn) {
+      if (usuario.rol === "Admin") {
+        navigate("/comunidad");
+      } else {
+        navigate("/farmacia");
+      }
     }
-  } else {
-    return (
-      <>
-        <Toaster />
-        <section className="bg-gray-50 dark:bg-gray-900 px-6 min-h-screen flex items-center justify-center h-screen -mt-[50px] sm:-mt-6">
-          <div className="w-full max-w-md bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-            <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-              <img
-                className="w-2/5 h-2/5 m-auto -mt-[65px] -mb-[20px] sm:-mb-[50px] sm:w-2/6"
-                src={comunidad}
-                alt="logo"
-              />
-            </a>
-            <div className="p-6 space-y-4">
-              <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Inicia Sesión en Comunidad San Francisco de Asís
-              </h1>
-              <div className="space-y-4">
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Tu nombre de usuario
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="PedroPerez"
-                    required=""
-                    autoComplete="nope"
-                    value={nombreUsuario}
-                    onChange={handleInputChangeUser}
-                  />
-                </div>
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Contraseña
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                    onChange={handleInputChangePassword}
-                    value={password}
-                  />
-                </div>
-                <Button
-                  onClick={obtenerTipoUsuario}
-                  className="w-full text-white bg-danger-600 hover:bg-danger-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Iniciar sesión
-                </Button>
+  }, [loggedIn, usuario]);
+
+  return (
+    <>
+      <Toaster />
+      <section className="bg-gray-50 dark:bg-gray-900 px-6 min-h-screen flex items-center justify-center h-screen -mt-[50px] sm:-mt-6">
+        <div className="w-full max-w-md bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+          <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img
+              className="w-2/5 h-2/5 m-auto -mt-[65px] -mb-[20px] sm:-mb-[50px] sm:w-2/6"
+              src={comunidad}
+              alt="logo"
+            />
+          </a>
+          <div className="p-6 space-y-4">
+            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Inicia Sesión en Comunidad San Francisco de Asís
+            </h1>
+            <div className="space-y-4">
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Tu nombre de usuario
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="PedroPerez"
+                  required=""
+                  autoComplete="nope"
+                  value={nombreUsuario}
+                  onChange={handleInputChangeUser}
+                />
               </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                  onChange={handleInputChangePassword}
+                  value={password}
+                />
+              </div>
+              <Button
+                onClick={obtenerTipoUsuario}
+                className="w-full text-white bg-danger-600 hover:bg-danger-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Iniciar sesión
+              </Button>
             </div>
           </div>
-        </section>
-      </>
-    );
-  }
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Login;
